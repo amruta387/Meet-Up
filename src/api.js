e385a49e8351630efb7978389c9b1a27d0845af2
Amruta <amruta.patil387@gmail.com>
2025-02-25 08:41:45 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -40,7 +40,7 @@ export const getEvents = async () => {
 
     if (token) {
         removeQuery();
-        const url = "YOUR_GET_EVENTS_API_ENDPOINT" + "/" + token;
+        const url = "https://s8szwyup2e.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
         const response = await fetch(url);
         const result = await response.json();
         if (result) {

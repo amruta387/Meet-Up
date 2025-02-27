14e303768c1fc4ed38b74215f69bc6ebb85c33ff
Amruta <amruta.patil387@gmail.com>
2025-02-27 12:04:45 +0100

    added attaus-spa

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from 'atatus-spa';
+
+atatus.config('2725307772844e41a0b3fabbee1cffa4').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+//atatus.notify(new Error('Test Atatus Setup'));

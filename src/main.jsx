2884a651a8e28d729f00faea5218862d429e5eef
Amruta <amruta.patil387@gmail.com>
2025-02-28 14:37:09 +0100

    commented atatus code

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
+//import * as atatus from 'atatus-spa';
 
-atatus.config('2725307772844e41a0b3fabbee1cffa4').install();
+//atatus.config('2725307772844e41a0b3fabbee1cffa4').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

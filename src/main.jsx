2d74f04560cdd76824307503dc0bffc38e55c36d
Amruta <amruta.patil387@gmail.com>
2025-03-01 13:43:58 +0100

    atatus configuration done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-//import * as atatus from 'atatus-spa';
+import * as atatus from 'atatus-spa';
 
-//atatus.config('2725307772844e41a0b3fabbee1cffa4').install();
+atatus.config('5e82b75a5d3e4c0a8048d4a012e93695').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-//atatus.notify(new Error('Test Atatus Setup'));
+atatus.notify(new Error('Test Atatus Setup'));

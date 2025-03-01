d3ea43d3c6255cfe5ab9cad706f9e782b3baf129
Amruta <amruta.patil387@gmail.com>
2025-03-01 17:08:43 +0100

    Update CityEventsChart.jsx

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -29,20 +29,23 @@ const CityEventsChart = ({ allLocations, events }) => {
 
     return (
         <ResponsiveContainer width="99%" height={400}>
-            <p>Events by City</p>
             <ScatterChart
                 margin={{
                     top: 20,
                     right: 20,
-                    bottom: 80,
+                    bottom: 60,
                     left: -30,
                 }}
             >
                 <CartesianGrid />
                 <XAxis
-          type="category" dataKey="city" name="City"
-          angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
-        />
+                    type="category"
+                    dataKey="city"
+                    name="City"
+                    angle={60}
+                    interval={0}
+                    tick={{ dx: 20, dy: 40, fontSize: 14 }}
+                />
                 <YAxis
                     type="number"
                     dataKey="count"

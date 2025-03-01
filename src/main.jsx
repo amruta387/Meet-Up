e7a4c321a6c66a9e901c7d34a40e620185f7112d
Amruta <amruta.patil387@gmail.com>
2025-03-01 14:07:41 +0100

    atatus config removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
+//import * as atatus from 'atatus-spa';
 
-try {
-  atatus.config(process.env.NEXT_PUBLIC_ATATUS_API_KEY).install();
-} catch (error) {
-  console.error("Atatus failed to initialize:", error);
-}
+//atatus.config('2725307772844e41a0b3fabbee1cffa4').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
 //atatus.notify(new Error('Test Atatus Setup'));
